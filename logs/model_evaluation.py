1d2fbce3f8392cbdf8693cb448a86dc7ae15617e
axitmehta <axitmehta6@gmail.com>
2025-08-24 11:56:43 +0530

    pipeline configured using dvc without params

--- logs/model_evaluation.py
+++ logs/model_evaluation.py
@@ -2,3 +2,7 @@
 2025-08-23 13:27:18,419 - DEBUG - model_evaluation - data loaded from ./data/processed/test_tfid.csv
 2025-08-23 13:27:18,452 - DEBUG - model_evaluation - model evaluation metrices calculated
 2025-08-23 13:27:18,453 - DEBUG - model_evaluation - metrices saved to reports/metrics.json
+2025-08-24 11:45:29,326 - DEBUG - model_evaluation - Model loaded successfully ./models/model.pkl
+2025-08-24 11:45:29,346 - DEBUG - model_evaluation - data loaded from ./data/processed/test_tfid.csv
+2025-08-24 11:45:29,380 - DEBUG - model_evaluation - model evaluation metrices calculated
+2025-08-24 11:45:29,383 - DEBUG - model_evaluation - metrices saved to reports/metrics.json
